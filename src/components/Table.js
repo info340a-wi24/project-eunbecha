ed02d74ea7d7531a60b1217dddc7a40cb917d4f1
eunbecha <95003795+eunbecha@users.noreply.github.com>
2024-02-29 22:02:37 -0800

    added playlist funcitonality

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,5 @@
 import React from 'react'; //import React library
+import { useState } from 'react';
 import { TableHeader } from './TableHeader';
 import { TableRow } from './TableRow';
 
@@ -6,20 +7,34 @@ import { TableRow } from './TableRow';
 
 function Table({props}) {
   let columnNames = ['Song', 'Artist', 'Url'];
+  const [songs, setSongs] = useState([]);
+
+  const handleAddButtonClick = () => {
+    let userInput = document.querySelector('.search-bar');
+    let userInputText = userInput.value;
+    setSongs(prevData => [...prevData, userInputText]);
+  }
   
-  let userInput = "UNDERSTAND"
-  let selectedData = props.filter(item => item.song === userInput)
-    .map(item => (
-      <TableRow key={item.song} props={item}/>
-    ));
+  let selectedData = props.filter(item => songs.includes(item.song))
+
+
+  let finalData = selectedData.map(item => (
+    <TableRow key={item.song} props={item}/>
+  ));
   
   return(
-    <table className='table table-bordered'>
+    <div>
+      <div>
+        <input type="text" className="search-bar" />
+        <button onClick={handleAddButtonClick}>+</button>
+      </div>
+      <table className='table table-bordered'>
       <TableHeader columnNames={columnNames}/>
       <tbody>
-        {selectedData}
+        {finalData}
       </tbody>
     </table>
+    </div>
   );
 }
 
