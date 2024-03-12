40afa9b833fc3c27ac6bb6b15acbdb023828639b
Ji_yoon_KIM <wbsk0810@naver.com>
2024-03-12 16:46:44 -0700

    login page done

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import { Routes, Route, Navigate, Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleSignIn = (event) => {
+        event.preventDefault();
+        try {
+            if (!email.includes('@')) {
+                console.error('Invalid email format');
+                return;
+            }
+            navigate('/');
+        } catch (error) {
+            console.error('Error during sign in:', error);
+        }
+    };
+
+
     return (
         <div>
             <div className="login-container">
                 <h1 id="logo_login"> <Link to="/">Musicify</Link></h1>
                 <p id="intro">Share music with all.</p>
-                <form>
-                    <label for="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="email" />
-                    <label for="pass">Password:</label>
-                    <input type="password" id="pass" name="password" placeholder="password" required />
-                    <button class="signin" type="signin" aria-label="SignIn">Sign In</button>
+                <form onSubmit={handleSignIn}>
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" placeholder="email"
+                    value={email} onChange={handleEmailChange} />
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" placeholder="password" required
+                    value={password} onChange={handlePasswordChange} />
+                    <button className="signin" type="submit" aria-label="SignIn">Sign In</button>
                 </form>
             </div>
         </div>
